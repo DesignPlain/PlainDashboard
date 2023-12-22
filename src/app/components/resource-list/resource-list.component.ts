7187429b88d5a2f202cd6e1649de48ee912fe780
nmbr7 <suhailr7@pm.me>
2023-12-23 00:50:03 +0530

    [Settings] Basic changes for settings popup

--- src/app/components/resource-list/resource-list.component.ts
+++ src/app/components/resource-list/resource-list.component.ts
@@ -12,6 +12,7 @@ export class ResourceListComponent {
   resourceType = ResourceType;
   viewAWS = false;
   viewGCP = false;
+  viewOnPremise = false;
   
 
   constructor(private _addComponentService: AddComponentService) {}
